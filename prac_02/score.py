6b79f0de9ed7462191c8939c87b96c5b594ebe2c
Zakia Adilah Khan <zakia.khan@my.jcu.edu.au>
2025-10-01 17:12:28 +1000

    Include docstrings for each function

--- prac_02/score.py
+++ prac_02/score.py
@@ -6,14 +6,16 @@ import random
 
 
 def main():
-    score = float(input("Enter score: "))
-    print(determine_score_status(score))
+    """Program to determine score status"""
+    user_score = float(input("Enter score: "))
+    print(determine_score_status(user_score))
 
     random_score = random.randint(0,100)
     print(f"Random score is: {random_score}")
     print(determine_score_status(random_score))
 
 def determine_score_status(score):
+    """Determine status of the score"""
     if score < 0 or score > 100:
         return("Invalid score")
     elif score >= 90:
