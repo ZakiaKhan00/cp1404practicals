e6e2dc7863f07c15ca046be0d9172d624b6b45cb
Zakia Adilah Khan <zakia.khan@my.jcu.edu.au>
2025-10-01 17:12:34 +1000

    Include docstrings for each function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -8,31 +8,33 @@ import random
 MENU = """ (G)et a valid score \n (P)rint result \n (S)how stars \n (Q)uit"""
 
 def main():
-    # score = get_valid_score()
+    """Main function for score menu program"""
+    score = get_valid_score()
     print(MENU)
     choice = input(">>> ").upper()
     while choice != 'Q':
         if choice == 'G':
             score = get_valid_score()
         elif choice == 'P':
-            print(f"Result: {determine_score_status(score)}")
+            print(f"Result: {determine_result(score)}")
         elif choice == 'S':
             show_stars(score)
         else:
             print("Invalid option")
-
         print(MENU)
         choice = input(">>> ").upper()
     print("Goodbye!")
 
 def get_valid_score():
+    """Get score from user between 0 to 100"""
     score = float(input("Enter score between 0 to 100: "))
     while score < 0 or score > 100:
         print("Invalid Score")
         score = float(input("Enter score between 0 to 100: "))
-    return (score)
+    return(score)
 
-def determine_score_status(score):
+def determine_result(score):
+    """Determine the result based on the score"""
     if score >= 90:
         return"Excellent"
     elif score >= 50:
@@ -41,6 +43,7 @@ def determine_score_status(score):
         return"Bad"
 
 def show_stars(score):
+    """Print asterisks for the length of the password"""
     print('*' * int(score))
 
 
