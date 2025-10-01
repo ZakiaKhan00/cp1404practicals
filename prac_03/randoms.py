cfbba3c7186de4ea0efbb0beaa80303e427c7e44
Zakia Adilah Khan <zakia.khan@my.jcu.edu.au>
2025-10-01 15:58:22 +1000

    Add random numbers comments and code

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+#Each time I run Line 1, it prints a random number between 5 and 20 inclusively.
+#Smallest: 5
+#Largest: 20
+
+print(random.randrange(3, 10, 2))  # line 2
+#Each time I run Line 2, it prints a random number either 3,5,7,9
+#Smallest: 3
+#Largest: 9
+#Could it produce 4? No, 4 doesn't fall in that sequence
+
+print(random.uniform(2.5, 5.5))  # line 3
+#Smallest: 2.5
+#Largest: 5.5
+
+print(random.randint(1,100))
