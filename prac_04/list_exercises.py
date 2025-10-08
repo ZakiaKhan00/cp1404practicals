144137ec93edc416f1dd6f01b99ddf2c8151304a
Zakia Adilah Khan <zakia.khan@my.jcu.edu.au>
2025-10-08 22:06:19 +1000

    Add list exercises program

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 Practical
+List Exercises
+"""
+from Demos.win32ts_logoff_disconnected import username
+
+#Part 1 Basic list operations
+numbers = []
+for i in range(5):
+    number = int(input(f"Number: "))
+    numbers.append(number)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+
+#Part 2: Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+if username is usernames:
+    print("Access granted")
+else:
+    print("Access denied")
