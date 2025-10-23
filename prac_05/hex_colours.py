c6eb66d7d7b8b1aaabcaf225a791491715484d00
Zakia Adilah Khan <zakia.khan@my.jcu.edu.au>
2025-10-23 16:02:55 +1000

    Split colour_codes dictionary into multiple lines for PEP 8 compliance

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,8 +4,9 @@ Hexadecimal colour codes lookup
 """
 
 # Constant dictionary of colour names and hexadecimal codes
-COLOUR_CODES = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "aqua": "#00ffff", "beige": "#f5f5dc", "coral": "#ff7f50",
-                               "crimson": "#dc143c", "gold": "#ffd700", "lavender": "#e6e6fa", "navy": "#000080", "salmon": "#fa8072"}
+COLOUR_CODES = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "aqua": "#00ffff",
+                "beige": "#f5f5dc", "coral": "#ff7f50","crimson": "#dc143c","gold": "#ffd700",
+                "lavender": "#e6e6fa", "navy": "#000080", "salmon": "#fa8072"}
 
 colour_name = input("Enter colour name: ").lower()
 while colour_name !="":
