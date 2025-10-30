70951842316e77d86b82f9529edc3808b65c35b9
Zakia Adilah Khan <zakia.khan@my.jcu.edu.au>
2025-10-30 22:13:23 +1000

    Add manual test to verify output for guitar age and vintage

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 Practical - Guitar class testing
+Estimate: 15 mins
+Actual: 10 mins
+"""
+
+from guitar import Guitar
+
+CURRENT_YEAR = 2025
+
+def run_tests():
+    """Tests for Guitar class methods."""
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
+
+    guitar = Guitar(name, year, cost)
+    other = Guitar("Another Guitar", 2012, 1512.9)
+
+    print(f"{guitar.name} get_age() - Expected {103}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {13}. Got {other.get_age()}")
+    print()
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
+
+
+if __name__ == '__main__':
+    run_tests()
