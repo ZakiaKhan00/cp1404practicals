082b16abe2dd671f323f35ffd9cf56c9a564b751
Zakia Adilah Khan <zakia.khan@my.jcu.edu.au>
2025-11-04 21:24:41 +1000

    Add lt method to sort guitars by year

---
+++ prac_07/guitar.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 Practical- Guitar class
+"""
+
+CURRENT_YEAR = 2025
+VINTAGE_AGE = 50
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a guitar instance"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a formatted string describing the guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Get age of guitar in years"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if guitar age is greater than 50"""
+        return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        """Display the list by year in sorted order (oldest to newest)."""
+        return self.year < other.year
